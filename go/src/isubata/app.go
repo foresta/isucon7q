bb1835466c5a0a29a0e3cff612840ed00a8ed68d
kazuki morita <kz.morit@gmail.com>
2018-07-02 13:27:05 +0900

    webapp: /fetchのスリープ期間を3廟に伸ばす

--- go/src/isubata/app.go
+++ go/src/isubata/app.go
@@ -521,7 +521,7 @@ func fetchUnread(c echo.Context) error {
 		return c.NoContent(http.StatusForbidden)
 	}
 
-	time.Sleep(time.Second)
+	time.Sleep(time.Second * 3)
 
 	query := `
 	SELECT channel.id AS channel_id,
